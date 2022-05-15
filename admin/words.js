3e2b1faa822ef6e47b917b81b3e807ac39c32f5a
Torsten Enkelmann <t.enkelmann@web.de>
2022-05-15 13:47:18 +0200

    Initail commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"bewaesserung adapter settings": {
+		"en": "Adapter settings for bewaesserung",
+		"de": "Adaptereinstellungen für bewaesserung",
+		"ru": "Настройки адаптера для bewaesserung",
+		"pt": "Configurações do adaptador para bewaesserung",
+		"nl": "Adapterinstellingen voor bewaesserung",
+		"fr": "Paramètres d'adaptateur pour bewaesserung",
+		"it": "Impostazioni dell'adattatore per bewaesserung",
+		"es": "Ajustes del adaptador para bewaesserung",
+		"pl": "Ustawienia adaptera dla bewaesserung",
+		"zh-cn": "bewaesserung的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "Option 1",
+		"ru": "Опция 1",
+		"pt": "Opção 1",
+		"nl": "Optie 1",
+		"fr": "Option 1",
+		"it": "opzione 1",
+		"es": "Opción 1",
+		"pl": "opcja 1",
+		"zh-cn": "选项1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "Option 2",
+		"ru": "вариант 2",
+		"pt": "opção 2",
+		"nl": "Optie 2",
+		"fr": "Option 2",
+		"it": "opzione 2",
+		"es": "opcion 2",
+		"pl": "Opcja 2",
+		"zh-cn": "选项2"
+	}
+};
